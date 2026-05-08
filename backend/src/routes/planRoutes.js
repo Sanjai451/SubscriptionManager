f87e8b1e23aad26ed606f398999e35706d98cfaa
Sanjai451 <sanjaikumarr451@gmail.com>
2026-05-08 10:39:54 +0530

    Updated frontend

--- backend/src/routes/planRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getPlans, getPlan } = require('../controllers/planController');
-
-router.get('/', getPlans);
-router.get('/:id', getPlan);
-
-module.exports = router;
